4c039a7e007bd27a83e002aac9d16619c797e79a
Josh <josh@redolive.com>
2019-06-08 21:13:34 -0600

    deleted cache

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('/Users/josh/sandbox/village-cleaners-gatsby/node_modules/gatsby-remark-images/gatsby-browser.js'),
-      options: {"plugins":[],"maxWidth":2048},
-    },{
-      plugin: require('/Users/josh/sandbox/village-cleaners-gatsby/node_modules/gatsby-plugin-netlify-cms/gatsby-browser.js'),
-      options: {"plugins":[],"modulePath":"/Users/josh/sandbox/village-cleaners-gatsby/src/cms/cms.js"},
-    }]
